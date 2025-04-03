99ce6e9dacff6d0c6e3e0a9db3bcc42378e54074
Rafael biano <164901522+rbiano@users.noreply.github.com>
2025-04-03 09:44:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+  botaoDeAcessibilidade.addEventListener('click', function (){
+  botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
     let tamanhoAtualFonte = 1;
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
@@ -16,11 +22,7 @@ document.addEventListener('DOMContentLoaded', function(){
       }
     })
   })
-  const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
-botaoDeAcessibilidade.addEventListener('click', function (){
-  botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-  opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+  
  })
   
 
